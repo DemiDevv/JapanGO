4a2a4ba284b2a5080804adc96befc816b37157e2
Demian Petropavlov <demyanpain@gmail.com>
2026-03-05 23:20:46 +0400

    Initial Commit

---
+++ JapanGo/App/JapanGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  JapanGoApp.swift
+//  JapanGo
+//
+//  Created by Demain Petropavlov on 05.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct JapanGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
